de9336d0a10e4dd60a77741ab6ecd0efdf568ae7
ijsbol <hi@uwu.gal>
2026-09-08 18:18:03 +0200

    feat: set nav button size based on button size

--- src/ticfyi/public/js/ring.js
+++ src/ticfyi/public/js/ring.js
@@ -25,7 +25,7 @@ function loadWebRing(domain) {
 
     // Keep the icon inline so embedding the ring does not require a CORS-enabled fetch.
     const button = `
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="32" height="32">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" height="100%">
             <path
                 d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
                    2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09
